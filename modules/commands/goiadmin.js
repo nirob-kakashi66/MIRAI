1a3a18499075843bc5f378f479d4d0dba79a3c07
🍒)⎯⃝𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃♡༏༏"🩷🐰༉༎🫰🫶 <130752960+BAYJID-404-BOYZ@users.noreply.github.com>
2024-12-06 23:03:01 +0600

    Update goiadmin.js

--- modules/commands/goiadmin.js
+++ modules/commands/goiadmin.js
@@ -2,18 +2,18 @@ module.exports.config = {
   name: "goiadmin",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "AYAN CHOUDHURY",
+  credits: "BAYJID",
   description: "bot admin mention",
   commandCategory: "Other",
   usages: "",
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "61557603311017") {
-    var aid = ["61557603311017"];
+  if (event.senderID !== "100005193854879") {
+    var aid = ["100005193854879"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Stop mentioning my creator, he's verry busy..!!😒", "AYAN Boss k mention na diya akta gf khuje deu boss sgl morse..!!😥", "Boss bow niya chipay gese..!!😪","AYAN boss busy ase pore dak diyo..!!😒","boss nai ki bolba amake bolte paro..!!😒"," kicce mention deu kn😒",];
+      var msg = ["Stop mentioning my creator, he's verry busy..!!😒", "BAYJID Boss k mention na diya akta gf khuje deu boss sgl morse..!!😥", "Boss bow niya chipay gese..!!😪","BAYJID boss busy ase pore dak diyo..!!😒","boss nai ki bolba amake bolte paro..!!😒"," kicce mention deu kn😒",];
       api.setMessageReaction("💐", event.messageID, (err) => {}, true);
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
