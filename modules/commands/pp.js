edbccb9332bdee51ad23ac2d2dbbfd10f7d90c27
🍒)⎯⃝𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃♡༏༏"🩷🐰༉༎🫰🫶 <130752960+BAYJID-404-BOYZ@users.noreply.github.com>
2024-12-07 09:38:36 +0600

    Update pp.js

--- modules/commands/pp.js
+++ modules/commands/pp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "AYAN",
+  credits: "BAYJID",
   description: "",
   commandCategory: "Tools",
   usages: "",
